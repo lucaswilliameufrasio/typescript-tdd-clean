ad2d930e91c421b3a261547e7946ca6c966f021c
Lucas William <lucaswilliameufrasio@gmail.com>
2020-07-16 09:58:48 -0300

    refactor: move CacheStoreSpy to its own file

---
+++ src/data/tests/mock-cache.ts
@@ -0,0 +1,33 @@
+import { ICacheStore } from "../protocols/cache";
+import { SavePurchases } from "@/domain/usecases";
+
+export class CacheStoreSpy implements ICacheStore {
+  deleteCallsCount = 0;
+  insertCallsCount = 0;
+  deleteKey: string;
+  insertKey: string;
+  insertValues: Array<SavePurchases.Params> = [];
+
+  delete(key): void {
+    this.deleteCallsCount++;
+    this.deleteKey = key;
+  }
+
+  insert(key: string, value: any): void {
+    this.insertCallsCount++;
+    this.insertKey = key;
+    this.insertValues = value;
+  }
+
+  simulateDeleteError(): void {
+    jest.spyOn(CacheStoreSpy.prototype, "delete").mockImplementationOnce(() => {
+      throw new Error();
+    });
+  }
+
+  simulateInsertError(): void {
+    jest.spyOn(CacheStoreSpy.prototype, "insert").mockImplementationOnce(() => {
+      throw new Error();
+    });
+  }
+}
--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -1,40 +1,5 @@
-import { ICacheStore } from "@/data/protocols/cache";
 import { LocalSavePurchases } from "@/data/usecases";
-import { SavePurchases } from "@/domain/usecases";
-import { mockPurchases } from "@/data/tests";
-
-class CacheStoreSpy implements ICacheStore {
-  deleteCallsCount = 0;
-  insertCallsCount = 0;
-  deleteKey: string;
-  insertKey: string;
-  insertValues: Array<SavePurchases.Params> = [];
-
-  delete(key): void {
-    this.deleteCallsCount++;
-    this.deleteKey = key;
-  }
-
-  insert(key: string, value: any): void {
-    this.insertCallsCount++;
-    this.insertKey = key;
-    this.insertValues = value;
-  }
-
-  simulateDeleteError(): void {
-    jest.spyOn(CacheStoreSpy.prototype, "delete").mockImplementationOnce(() => {
-      throw new Error();
-    });
-  }
-
-  simulateInsertError(): void {
-    jest.spyOn(CacheStoreSpy.prototype, "insert").mockImplementationOnce(() => {
-      throw new Error();
-    });
-  }
-}
-
-
+import { mockPurchases, CacheStoreSpy } from "@/data/tests";
 
 type SutTypes = {
   sut: LocalSavePurchases;
